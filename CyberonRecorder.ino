8022b1fbf54643d0bdb8fdd239fcdb4d6f6ad39d
taihsuanho <tai@cyberon.com.tw>
2017-05-17 21:31:26 +0800

    Recording up to 32kHz

--- CyberonRecorder.ino
+++ CyberonRecorder.ino
@@ -1,25 +1,28 @@
   
 #define BAUD_RATE         921600
-#define FRAME_SIZE        8    // Has to be power of 2 for fast mod(%) operation.
+#define FRAME_SIZE        4    // Has to be power of 2 for fast mod(%) operation.
 #define HALF_SIZE         (FRAME_SIZE >> 1)
 #define HALF_BUF_SIZE     (HALF_SIZE * 3)
 
 #define TICKS_8KHZ        1920  // Should be 16MHz/prescale/8kHz = 2000, but the tone sounds higher.
 #define TICKS_11KHZ       1320
 #define TICKS_16KHZ       880
-#define DC_OFFSET         0
+#define TICKS_22KHZ       620
+#define TICKS_32KHZ       420
 
 #define CMD_START         0x31  // Start recording
 #define CMD_STOP          0x32  // Stop recording
 #define CMD_8KHZ          0x41  // Set 8kHz sampling rate
-#define CMD_11KHZ         0x42  // Set 11kHz sampling rate
-#define CMD_16KHZ         0x43  // Set 16kHz sampling rate
+#define CMD_11KHZ         0x42  
+#define CMD_16KHZ         0x43  
+#define CMD_22KHZ         0x44  
+#define CMD_32KHZ         0x45  
 
 #define MIC_PIN           A1
 
 bool flagRecord = false, flag0 = false, flag1 = false;
 int count = 0;
-char buf[FRAME_SIZE * 3];  // for 16-bit sample plus 8-bit checksum
+char buf[HALF_BUF_SIZE * 2];
 
 void freeRunningModeADC(int micPin)
 {
@@ -33,9 +36,9 @@ void freeRunningModeADC(int micPin)
   // for Arduino Uno ADC clock is 16 MHz and a conversion takes 13 clock cycles
   ADCSRA |= (1 << ADPS2);             // 16 prescaler for 76.9 KHz
 
-  ADCSRA |= (1 << ADATE); // enable auto trigger
-  ADCSRA |= (1 << ADEN);  // enable ADC
-  ADCSRA |= (1 << ADSC);  // start ADC measurements
+  ADCSRA |= (1 << ADATE);             // enable auto trigger
+  ADCSRA |= (1 << ADEN);              // enable ADC
+  ADCSRA |= (1 << ADSC);              // start ADC measurements
 }
 
 void addSample()
@@ -59,6 +62,8 @@ void addSample()
 void setup()
 {
   Serial.begin(BAUD_RATE);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
   pinMode(MIC_PIN, INPUT);
   freeRunningModeADC(MIC_PIN);
   init_timer1_prescale1(TICKS_16KHZ, addSample);
@@ -85,24 +90,45 @@ void loop()
 
 void serialEvent()
 {
-  char c = Serial.read();
+  static int nIdxCmd = 0;
+  static char cmdBuf[4];
 
-  if (!flagRecord && c == CMD_START){
-      count = 0;
-      flag0 = flag1 = false;
-      flagRecord = true;
-  }
-  else if (flagRecord && c == CMD_STOP) {
-    flagRecord = false;
-  }
-  else if (c == CMD_8KHZ){
-    init_timer1_prescale1(TICKS_8KHZ, addSample);
-  }
-  else if (c == CMD_11KHZ){
-    init_timer1_prescale1(TICKS_11KHZ, addSample);
-  }
-  else if (c == CMD_16KHZ){
-    init_timer1_prescale1(TICKS_16KHZ, addSample);
+  while(Serial.available()){
+    cmdBuf[nIdxCmd++] = Serial.read();
+    if (nIdxCmd == 4){
+      if (strncmp(cmdBuf, "CYB", 3) != 0){
+        memmove(cmdBuf, cmdBuf + 1, 3); 
+        nIdxCmd = 3;
+      }
+      else{
+        if (!flagRecord && cmdBuf[3] == CMD_START){
+          count = 0;
+          flag0 = flag1 = false;
+          flagRecord = true;
+          digitalWrite(12, HIGH);
+        }
+        else if (flagRecord && cmdBuf[3] == CMD_STOP) {
+          flagRecord = false;
+          digitalWrite(12, LOW);
+        }
+        else if (cmdBuf[3] == CMD_8KHZ){
+          init_timer1_prescale1(TICKS_8KHZ, addSample);
+        }
+        else if (cmdBuf[3] == CMD_11KHZ){
+          init_timer1_prescale1(TICKS_11KHZ, addSample);
+        }
+        else if (cmdBuf[3] == CMD_16KHZ){
+          init_timer1_prescale1(TICKS_16KHZ, addSample);
+        }
+        else if (cmdBuf[3] == CMD_22KHZ){
+          init_timer1_prescale1(TICKS_22KHZ, addSample);
+        }
+        else if (cmdBuf[3] == CMD_32KHZ){
+          init_timer1_prescale1(TICKS_32KHZ, addSample);
+        }
+        nIdxCmd = 0;
+      }
+    }
   }
 }
 
